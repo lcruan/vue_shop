a3cd043ae9d666e1a7fb63692181f56d99c33beb
lcruan <1416802331@qq.com>
2022-08-05 22:20:41 +0800

    商品新增

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -30,7 +30,9 @@ import { Button,
          Cascader,
          Alert,
          Tabs,
-         TabPane 
+         TabPane,
+         Steps,
+         Step 
         } from 'element-ui'
 
 Vue.use(Button)
@@ -63,5 +65,7 @@ Vue.use(Cascader)
 Vue.use(Alert)
 Vue.use(Tabs)
 Vue.use(TabPane)
+Vue.use(Steps)
+Vue.use(Step)
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
