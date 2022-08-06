4c9104f07733ee89b9b2ec45afa21afbe6f7a8e9
lcruan <1416802331@qq.com>
2022-08-07 07:38:03 +0800

    完成订单功能开发

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -35,7 +35,9 @@ import { Button,
          Step,
          CheckboxGroup,
          Checkbox,
-         Upload 
+         Upload,
+         Timeline,
+         TimelineItem
         } from 'element-ui'
 
 Vue.use(Button)
@@ -73,5 +75,7 @@ Vue.use(Step)
 Vue.use(CheckboxGroup)
 Vue.use(Checkbox)
 Vue.use(Upload)
+Vue.use(Timeline)
+Vue.use(TimelineItem)
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
