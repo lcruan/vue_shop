57f2d2598cf9636548fa3145f681822b80d0b518
lcruan <1416802331@qq.com>
2022-08-06 18:52:43 +0800

    完成商品功能开发

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -32,7 +32,10 @@ import { Button,
          Tabs,
          TabPane,
          Steps,
-         Step 
+         Step,
+         CheckboxGroup,
+         Checkbox,
+         Upload 
         } from 'element-ui'
 
 Vue.use(Button)
@@ -67,5 +70,8 @@ Vue.use(Tabs)
 Vue.use(TabPane)
 Vue.use(Steps)
 Vue.use(Step)
+Vue.use(CheckboxGroup)
+Vue.use(Checkbox)
+Vue.use(Upload)
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
